e694332186d2c2a0e9841f7cf6850aa9907869f1
kris fox <kris.fox@gmail.com>
2011-03-08 09:47:52 -0800

    took out hard coded styles

--- javascript/collapsable_table.js
+++ javascript/collapsable_table.js
@@ -8,10 +8,10 @@
 						function() {
 							var row = $(this);
 							row.addClass('indent_' + (row.parents('.children').length + 1));
-							row.children(':first-child').css('padding', '0').css('border', 'none');
+							row.children(':first-child').addClass('collapsed_row');
 							$('table tr td:first-child', row).addClass('indent');
 							row.prev().children("tr > td:first-child").prepend("<a class='expandable arrow collapsed' style='cursor:pointer'>+</a>");
-							row.find(":first-child table").css('margin', '0').addClass('collapsed_table').hide();
+							row.find(":first-child table").addClass('collapsed_table').hide();
 						}
 					);
 					$('a.expandable.arrow', $this).click(function() {
